659831782dccf55267529d7876c9d3aa1c25930b
phillipkregg <phillipkregg@gmail.com>
2012-12-31 19:41:02 -0600

    Testing javascript

---
+++ public/javascript/custom.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+	
+	$("#eastBtn").bind('click', function() {
+		
+		$.scrollTo("#regionMidwest", 800);
+		
+	});
+	
+	$("#westBtn").bind('click', function() {
+		
+		$.scrollTo("#regionEast", 800);
+		
+	});
+	
+		
+	
+	
+	
+	
+	
+});
